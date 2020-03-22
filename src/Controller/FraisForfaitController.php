8ed883396e2f543193ae50af2fd515f2422e8108
FlorianMartinez <florianmartinez98@outlook.com>
2020-03-22 19:54:06 +0100

    Création de la table Etat + génération de son crud

--- src/Controller/FraisForfaitController.php
+++ src/Controller/FraisForfaitController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/frais/forfait")
+ * @Route("/frais_forfait")
  */
 class FraisForfaitController extends AbstractController
 {
---
+++ src/Entity/Etat.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\EtatRepository")
+ */
+class Etat
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $abreviation;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $libelle;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getAbreviation(): ?string
+    {
+        return $this->abreviation;
+    }
+
+    public function setAbreviation(string $abreviation): self
+    {
+        $this->abreviation = $abreviation;
+
+        return $this;
+    }
+
+    public function getLibelle(): ?string
+    {
+        return $this->libelle;
+    }
+
+    public function setLibelle(string $libelle): self
+    {
+        $this->libelle = $libelle;
+
+        return $this;
+    }
+}
